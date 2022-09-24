2f3e2730404cef59c3bce5ef332adc2d8f768e3f
ricardocolom64 <ricardocolom64@gmail.com>
2022-09-24 18:46:09 -0400

    quick prototypes of dark mode in weigthcalculator

--- components/WeightCalculator.js
+++ components/WeightCalculator.js
@@ -2,14 +2,25 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { Dimensions, Keyboard, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
-import { Box, Input, InputGroup, InputRightAddon } from 'native-base';
+import { Box, Input, InputGroup, InputRightAddon, Button, useColorMode, useColorModeValue } from 'native-base';
 
 var allWeights = [45, 25, 10, 5, 2.5];
 var barWeight = 45;
 
+const lightBorder = "#D4D4D4";
+const darkBorder = "#404040";
+
+const lightBg = "#F5F5F5";
+const darkBg = "#262626";
+
 export default function WeightCalculator() {
     const [inpWeight, setInpWeight] = useState(0);
     const [weights, setWeights] = useState("");
+
+    const {
+        colorMode,
+        toggleColorMode
+    } = useColorMode();
 
 
     function calcWeights() {
@@ -47,13 +58,13 @@ export default function WeightCalculator() {
     return (
         <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
             <View style={styles.main}>
-                <Box style={styles.mainContent} bg="white" rounded="8" shadow={0} borderWidth="1" borderColor="coolGray.300">
-                    <View style={styles.header}>
-                        <Text style={styles.headerText}>Weight Calculator</Text>
+                <Box style={styles.mainContent} bg={useColorModeValue("white", "#151e31")} rounded="8" shadow={0} borderWidth="1" borderColor="coolGray.300">
+                    <View style={styles.header} borderColor={useColorModeValue(lightBorder, darkBorder)}>
+                        {/* <Button onPress={toggleColorMode}>Toggle</Button> */}
                     </View>
                     <View style={styles.inpAndOut}>
-                        <View style={styles.output}>
-                            <Text style={styles.outputText}>{weights}</Text>
+                        <View style={styles.output} borderColor={useColorModeValue(lightBorder, darkBorder)}>
+                            <Text style={[styles.outputText, { color: useColorModeValue("black", "white") }]}>{weights}</Text>
                         </View>
                         <View style={styles.input}>
                             <InputGroup position={"absolute"} right={0}>
@@ -66,7 +77,7 @@ export default function WeightCalculator() {
                                     value={inpWeight}
                                     keyboardType={'numeric'}
                                 />
-                                <InputRightAddon children={"lb"}/>
+                                <InputRightAddon children={"lb"} bgColor={useColorModeValue(lightBg, darkBg)} borderColor={useColorModeValue(lightBorder, darkBorder)} />
                             </InputGroup>
                         </View>
                     </View>
